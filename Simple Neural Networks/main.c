af706f1f83e8f741dabbe6d56626e493bd5189a2
HanielUlises <hanielulises2003@gmail.com>
2024-04-09 21:23:18 -0600

    Simple neural network

--- Simple Neural Networks/main.c
+++ Simple Neural Networks/main.c
@@ -4,11 +4,17 @@
 #include "simple_nn.h"
 
 void test_nn(){
-    double temperature[] = {12,23,50,-10,16};
+    double temperature[] = {12, 23, 50, -10, 16};
     double weight = -2;
 
-    printf("The first predicted value is %d: \r\n", single_in_single_out(temperature[0],weight));
+    size_t vec_size = sizeof(temperature) / sizeof(double);
+
+    printf("Predicted values: \n");
+    for(size_t i = 0; i < vec_size; i++){
+        printf("Current predicted value %zu: %f\n", i, single_in_single_out(temperature[i], weight));
+    }
 }
+
 
 int main (){
     test_nn();
