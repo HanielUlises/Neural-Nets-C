ac94cabe207d5a41f5abbd9782942b22dcd2f048
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2024-04-18 21:44:51 -0600

    Multiple input multiple output

--- Simple Neural Networks/simple_nn.c
+++ Simple Neural Networks/simple_nn.c
@@ -1,36 +1,43 @@
 #include "simple_nn.h"
+#include <stddef.h>
 
-
-double single_in_single_out (double input, double weight){
-    return(input * weight);
+double single_in_single_out(double input, double weight) {
+    return input * weight;
 }
 
-double multiple_in_single_out(double* input, double* weight, int length){
-    double predicted_value;
-
-    predicted_value = weighted_sum(input, weight, length);
+double multiple_in_single_out(double* input, double* weight, int length) {
+    return weighted_sum(input, weight, length);
 }
 
-double weighted_sum (double* input, double* weight, int length){
-    double output;
+double weighted_sum(double* input, double* weight, int length) {
+    double output = 0.0;
 
-    for(size_t i = 0; i < length; i++){
+    for (size_t i = 0; i < length; i++) {
         output += input[i] * weight[i];
     }
 
     return output;
 }
 
-void element_wise_multiply (double input_scalar, double* weight_vector, double* output_vector, int length){
-    for(size_t i = 0; i < length; i++){
+void element_wise_multiply(double input_scalar, double* weight_vector, double* output_vector, int length) {
+    for (size_t i = 0; i < length; i++) {
         output_vector[i] = input_scalar * weight_vector[i];
     }
 }
 
-void single_in_multiple_out (double scalar, double* w_vect, double* out_vect, int length){
+void single_in_multiple_out(double scalar, double* w_vect, double* out_vect, int length) {
     element_wise_multiply(scalar, w_vect, out_vect, length);
 }
 
-void multiple_in_multiple_out(double *input_vector, int INPUT_LEN, double *output_vector, int OUTPUT_LEN, double weight_matrix[INPUT_LEN][OUTPUT_LEN]){
+void multiple_in_multiple_out(double *input_vector, int INPUT_LEN, double *output_vector, int OUTPUT_LEN, double **weight_matrix) {
+    for (int i = 0; i < OUTPUT_LEN; i++) {
+        output_vector[i] = 0;
+        for (int j = 0; j < INPUT_LEN; j++) {
+            output_vector[i] += input_vector[j] * weight_matrix[j][i];
+        }
+    }
+}
 
+void matrix_vector_multiply(double *input_vector, int INPUT_LEN, double *output_vector, int OUTPUT_LEN, double **weight_matrix) {
+    multiple_in_multiple_out(input_vector, INPUT_LEN, output_vector, OUTPUT_LEN, weight_matrix);
 }
--- Simple Neural Networks/simple_nn.h
+++ Simple Neural Networks/simple_nn.h
@@ -3,14 +3,13 @@
 
 #include <stdlib.h>
 
-double single_in_single_out (double input, double weight);
+double single_in_single_out(double input, double weight);
 double multiple_in_single_out(double *input, double *weight, int length);
-void single_in_multiple_out (double scalar, double *w_vect, double *ut_vect, int length);
-void multiple_in_multiple_out(double *input_vector, int INPUT_LEN, double *output_vector, int OUTPUT_LEN, double weight_matrix[INPUT_LEN][OUTPUT_LEN]);
+void single_in_multiple_out(double scalar, double *w_vect, double *ut_vect, int length);
+void multiple_in_multiple_out(double *input_vector, int INPUT_LEN, double *output_vector, int OUTPUT_LEN, double **weight_matrix);
 
-double weighted_sum (double *input, double *weight, int length);
-void element_wise_multiply (double input_scalar, double *weight_vector, double *output_vector, int length);
-void matrix_vector_multiply (double *input_vector, const int INPUT_LEN, double *output_vector, const int OUTPUT_LEN, double weight_matrix[OUTPUT_LEN][INPUT_LEN]);
-
+double weighted_sum(double *input, double *weight, int length);
+void element_wise_multiply(double input_scalar, double *weight_vector, double *output_vector, int length);
+void matrix_vector_multiply(double *input_vector, int INPUT_LEN, double *output_vector, int OUTPUT_LEN, double **weight_matrix);
 
 #endif
