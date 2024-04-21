612cfe09a1320cab90b86f6fc1112007075d585f
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2024-04-21 17:52:21 -0600

    Hidden layer neural network

--- Simple Neural Networks/simple_nn.c
+++ Simple Neural Networks/simple_nn.c
@@ -9,6 +9,29 @@ double multiple_in_single_out(double* input, double* weight, int length) {
     return weighted_sum(input, weight, length);
 }
 
+void single_in_multiple_out(double scalar, double* w_vect, double* out_vect, int length) {
+    element_wise_multiply(scalar, w_vect, out_vect, length);
+}
+
+void multiple_in_multiple_out(double *input_vector, int INPUT_LEN, double *output_vector, int OUTPUT_LEN, double weight_matrix[][OUTPUTS]) {
+    for (int i = 0; i < OUTPUT_LEN; i++) {
+        output_vector[i] = 0;
+        for (int j = 0; j < INPUT_LEN; j++) {
+            output_vector[i] += input_vector[j] * weight_matrix[j][i];
+        }
+    }
+}
+
+void hidden_layer_nn(double *input_vector, double in_to_hid_weights[HIDDEN_SIZE][INPUT_SIZE], double hid_to_out_weights[OUTPUT_SIZE][HIDDEN_SIZE], double *output_vector){
+    double hidden_pred_vector[HIDDEN_SIZE];
+    matrix_vector_multiplication(input_vector, INPUT_SIZE, hidden_pred_vector, HIDDEN_SIZE, in_to_hid_weights);
+    matrix_vector_multiplication(hidden_pred_vector, HIDDEN_SIZE, output_vector, OUTPUT_SIZE, output_vector);
+}
+
+void matrix_vector_multiplication (double *input_vector, int INPUT_LEN, double *output_vector, int OUTPUT_LEN, double weight_matrix[][OUTPUTS]) {
+    multiple_in_multiple_out(input_vector, INPUT_LEN, output_vector, OUTPUT_LEN, weight_matrix);
+}
+
 double weighted_sum(double* input, double* weight, int length) {
     double output = 0.0;
 
@@ -23,21 +46,4 @@ void element_wise_multiply(double input_scalar, double* weight_vector, double* o
     for (size_t i = 0; i < length; i++) {
         output_vector[i] = input_scalar * weight_vector[i];
     }
-}
-
-void single_in_multiple_out(double scalar, double* w_vect, double* out_vect, int length) {
-    element_wise_multiply(scalar, w_vect, out_vect, length);
-}
-
-void multiple_in_multiple_out(double *input_vector, int INPUT_LEN, double *output_vector, int OUTPUT_LEN, double weight_matrix[][OUTPUTS]) {
-    for (int i = 0; i < OUTPUT_LEN; i++) {
-        output_vector[i] = 0;
-        for (int j = 0; j < INPUT_LEN; j++) {
-            output_vector[i] += input_vector[j] * weight_matrix[j][i];
-        }
-    }
-}
-
-void matrix_vector_multiply(double *input_vector, int INPUT_LEN, double *output_vector, int OUTPUT_LEN, double weight_matrix[][OUTPUTS]) {
-    multiple_in_multiple_out(input_vector, INPUT_LEN, output_vector, OUTPUT_LEN, weight_matrix);
 }
--- Simple Neural Networks/simple_nn.h
+++ Simple Neural Networks/simple_nn.h
@@ -6,14 +6,26 @@
 #define NUM_FEATURES 3
 #define OUTPUTS 3
 
+#define HIDDEN_SIZE 3
+#define INPUT_SIZE 3
+#define OUTPUT_SIZE 3
 
+// Neural networks
 double single_in_single_out(double input, double weight);
 double multiple_in_single_out(double *input, double *weight, int length);
 void single_in_multiple_out(double scalar, double *w_vect, double *ut_vect, int length);
 void multiple_in_multiple_out(double *input_vector, int INPUT_LEN, double *output_vector, int OUTPUT_LEN, double weight_matrix[][OUTPUTS]);
 
+
+void hidden_layer_nn(double *input_vector, 
+                     double in_to_hid_weights[HIDDEN_SIZE][INPUT_SIZE], 
+                     double hid_to_out_weights[OUTPUT_SIZE][HIDDEN_SIZE], 
+                     double *output_vector);
+
+
+// Utils
 double weighted_sum(double *input, double *weight, int length);
 void element_wise_multiply(double input_scalar, double *weight_vector, double *output_vector, int length);
-void matrix_vector_multiply(double *input_vector, int INPUT_LEN, double *output_vector, int OUTPUT_LEN, double weight_matrix[][OUTPUTS]);
+void matrix_vector_multiplication(double *input_vector, int INPUT_LEN, double *output_vector, int OUTPUT_LEN, double weight_matrix[][OUTPUTS]);
 
 #endif
